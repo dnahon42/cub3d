8e393f39010a14eda5bbb5cc9f4bd6ab9515d67f
Killianb0101010 <teixeira.killian44600@gmail.com>
2025-09-13 19:01:14 +0200

    point afficher

--- include/cube3d.h
+++ include/cube3d.h
@@ -3,22 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   cube3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dnahon <dnahon@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kiteixei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 15:51:38 by kiteixei          #+#    #+#             */
-/*   Updated: 2025/09/12 16:36:20 by dnahon           ###   ########.fr       */
+/*   Updated: 2025/09/13 18:13:44 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../minilibx-linux/mlx.h"
-#include "../libft/includes/libft.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#ifndef CUBE3D_H
+# define CUBE3D_H
 
-#define RESET "\033[0m"
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define CYAN "\033[36m"
-#define BOLD "\033[1m"
+# include "../minilibx-linux/mlx.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef KEY_LEFT
+#  define KEY_LEFT 97
+# endif
+# ifndef KEY_RIGHT
+#  define KEY_RIGHT 100
+# endif
+# ifndef KEY_UP
+#  define KEY_UP 119
+# endif
+# ifndef KEY_DOWN
+#  define KEY_DOWN 115
+# endif
+# ifndef KEY_EXIT
+#  define KEY_EXIT 65307
+# endif
+
+typedef struct s_map
+{
+	int		flag_w;
+	int		flag_s;
+	int		flag_a;
+	int		flag_d;
+	int		flag_exit;
+	float	pos_y;
+	float	pos_x;
+	float	new_pos_y;
+	float	new_pos_x;
+	int		x;
+	int		y;
+	void	*mlx;
+	void	*win;
+	void	*player;
+	int		img_width;
+	int		img_height;
+}			t_map;
+#endif
--- main.c
+++ main.c
@@ -6,18 +6,117 @@
 /*   By: dnahon <dnahon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 15:43:18 by kiteixei          #+#    #+#             */
-/*   Updated: 2025/09/12 16:36:47 by dnahon           ###   ########.fr       */
+/*   Updated: 2025/09/13 18:36:25 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/cube3d.h"
+#include "minilibx-linux/mlx.h"
 
-int validate_args(int ac, char **av)
+void	draw_player(t_map *map)
 {
-    if (ac != 3)
-        ft_putstr_fd();
+	map->pos_x = 1;
+	map->pos_y = 1;
+	map->player = mlx_xpm_file_to_image(map->mlx, "player/point.xpm",
+			&map->img_height, &map->img_width);
+	mlx_put_image_to_window(map->mlx, map->win, map->player, map->pos_x * 10,
+		map->pos_y * 10);
+}
+
+void	ft_moove(t_map *map)
+{
+	map->new_pos_x = map->pos_x;
+	map->new_pos_y = map->pos_y;
+	if (map->flag_w == 1)
+		map->new_pos_y--;
+	if (map->flag_s == 1)
+		map->new_pos_y++;
+	if (map->flag_a == 1)
+		map->new_pos_x--;
+	if (map->flag_d == 1)
+		map->new_pos_x++;
+}
+
+void	init_flag(t_map *map)
+{
+	map->flag_w = 0;
+	map->flag_s = 0;
+	map->flag_a = 0;
+	map->flag_d = 0;
+}
+
+void	render_player(t_map *map)
+{
+	mlx_clear_window(map->mlx, map->win);
+	mlx_put_image_to_window(map->mlx, map->win, map->player, map->pos_x * 10,
+		map->pos_y * 10);
+}
+
+int	refresh(void *param)
+{
+	t_map	*map;
+
+	map = (t_map *)param;
+	ft_moove(map);
+	init_flag(map);
+	map->pos_x = map->new_pos_x;
+	map->pos_y = map->new_pos_y;
+	render_player(map);
+	return (0);
+}
+
+int	key_press(int keycode, void *param)
+{
+	t_map	*map;
+
+	map = (t_map *)param;
+	if (keycode == KEY_UP)
+		map->flag_w = 1;
+	else if (keycode == KEY_DOWN)
+		map->flag_s = 1;
+	else if (keycode == KEY_LEFT)
+		map->flag_a = 1;
+	else if (keycode == KEY_RIGHT)
+		map->flag_d = 1;
+	else if (keycode == KEY_EXIT)
+		exit(1);
+	return (refresh(map));
+}
+
+int	key_release(int keycode, void *param)
+{
+	t_map	*map;
+
+	map = (t_map *)param;
+	if (keycode == KEY_LEFT)
+		map->flag_a = 0;
+	else if (keycode == KEY_RIGHT)
+		map->flag_d = 0;
+	else if (keycode == KEY_UP)
+		map->flag_w = 0;
+	else if (keycode == KEY_DOWN)
+		map->flag_s = 0;
+	else if (keycode == KEY_EXIT)
+		map->flag_exit = 0;
+	return (refresh(map));
+}
+
+void	ft_exec(t_map *map)
+{
+	mlx_hook(map->win, 2, 1, key_press, map);
+	mlx_hook(map->win, 3, 1, key_release, map);
+	render_player(map);
+	mlx_loop(map->mlx);
 }
 
 int	main(void)
 {
+	t_map(map) = {0};
+	map.mlx = mlx_init();
+	if (!map.mlx)
+		return (1);
+	map.win = mlx_new_window(map.mlx, 1000, 1000, "Cub3d");
+	draw_player(&map);
+	ft_exec(&map);
+	mlx_loop(map.mlx);
 }
